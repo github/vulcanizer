554cf711057fcd1da1ee96af5800aa25c3cf58fa
Jess Breckenridge <jessbreckenridge@users.noreply.github.com>
2018-08-29 18:03:47 -0600

    - First pass at table tests complete, swapping file names to clean this up. - Added `gotest` abilities if file is present for colorized output.

--- pkg/vulcanizer/es_test.go
+++ pkg/vulcanizer/es_test.go
@@ -2,43 +2,19 @@ package vulcanizer
 
 import (
 	"fmt"
+	"io/ioutil"
 	"net/http"
+	"net/http/httptest"
+	"net/url"
+	"strconv"
 	"testing"
 )
 
-//
-// type ServerSetup struct {
-// 	Method, Path, Body, Response string
-// 	HttpStatus                   int
-// }
-//
-// func setupTestServers(t *testing.T, setups []*ServerSetup) (string, int, *httptest.Server) {
-// 	ts := httptest.NewServer(http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
-//
-// 		requestBytes, _ := ioutil.ReadAll(r.Body)
-// 		requestBody := string(requestBytes)
-//
-// 		matched := false
-// 		for _, setup := range setups {
-// 			if r.Method == setup.Method && r.URL.EscapedPath() == setup.Path && requestBody == setup.Body {
-// 				matched = true
-// 				if setup.HttpStatus == 0 {
-// 					w.WriteHeader(http.StatusOK)
-// 				} else {
-// 					w.WriteHeader(setup.HttpStatus)
-// 				}
-// 				w.Write([]byte(setup.Response))
-// 			}
-// 		}
-//
-// 		if matched == false {
-// 			t.Fatalf("No requests matched setup. Got method %s, Path %s, body %s", r.Method, r.URL.EscapedPath(), requestBody)
-// 		}
-// 	}))
-// 	url, _ := url.Parse(ts.URL)
-// 	port, _ := strconv.Atoi(url.Port())
-// 	return url.Hostname(), port, ts
-// }
+// ServerSetup type contains the Method, Path, Body and Response strings, as well as the HTTP Status code.
+type ServerSetup struct {
+	Method, Path, Body, Response string
+	HTTPStatus                   int
+}
 
 func TestGetClusterExcludeSettings(t *testing.T) {
 
@@ -281,99 +257,173 @@ func TestGetSettings(t *testing.T) {
 	}
 }
 
-// func TestSetSetting_ExistingTransientSetting(t *testing.T) {
-// 	getSetup := &ServerSetup{
-// 		Method:   "GET",
-// 		Path:     "/_cluster/settings",
-// 		Response: `{"persistent":{},"transient":{"cluster":{"routing":{"allocation":{"exclude":{"_name":"10.0.0.2"}}}}}}`,
-// 	}
-// 	putSetup := &ServerSetup{
-// 		Method:   "PUT",
-// 		Path:     "/_cluster/settings",
-// 		Body:     `{"transient":{"cluster.routing.allocation.exclude._name":"10.0.0.99"}}`,
-// 		Response: `{"persistent":{},"transient":{"cluster":{"routing":{"allocation":{"exclude":{"_name":"10.0.0.99"}}}}}}`,
-// 	}
+// setupTestServer sets up an HTTP test server to serve data to the test that come after it.
 
-// 	host, port, ts := setupTestServers(t, []*ServerSetup{getSetup, putSetup})
-// 	defer ts.Close()
+func setupTestServers(t *testing.T, setups []*ServerSetup) (string, int, *httptest.Server) {
 
-// 	oldValue, newValue, err := SetSetting(host, port, "cluster.routing.allocation.exclude._name", "10.0.0.99")
+	ts := httptest.NewServer(http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
 
-// 	if err != nil {
-// 		t.Fatalf("Expected error to be nil, %s", err)
-// 	}
+		requestBytes, _ := ioutil.ReadAll(r.Body)
+		requestBody := string(requestBytes)
 
-// 	if oldValue != "10.0.0.2" {
-// 		t.Fatalf("Unexpected old value, got %s", oldValue)
-// 	}
+		matched := false
+		for _, setup := range setups {
+			if r.Method == setup.Method && r.URL.EscapedPath() == setup.Path && requestBody == setup.Body {
+				matched = true
+				if setup.HTTPStatus == 0 {
+					w.WriteHeader(http.StatusOK)
+				} else {
+					w.WriteHeader(setup.HTTPStatus)
+				}
+				w.Write([]byte(setup.Response))
+			}
+		}
 
-// 	if newValue != "10.0.0.99" {
-// 		t.Fatalf("Unexpected new value, got %s", newValue)
-// 	}
-// }
+		if matched == false {
+			t.Fatalf("No requests matched setup. Got method %s, Path %s, body %s", r.Method, r.URL.EscapedPath(), requestBody)
+		}
+	}))
+	url, _ := url.Parse(ts.URL)
+	port, _ := strconv.Atoi(url.Port())
+	return url.Hostname(), port, ts
+}
 
-// func TestSetSetting_ExistingPersistentSetting(t *testing.T) {
-// 	getSetup := &ServerSetup{
-// 		Method:   "GET",
-// 		Path:     "/_cluster/settings",
-// 		Response: `{"transient":{},"persistent":{"cluster":{"routing":{"allocation":{"exclude":{"_name":"10.0.0.2"}}}}}}`,
-// 	}
-// 	putSetup := &ServerSetup{
-// 		Method:   "PUT",
-// 		Path:     "/_cluster/settings",
-// 		Body:     `{"transient":{"cluster.routing.allocation.exclude._name":"10.0.0.99"}}`,
-// 		Response: `{"persistent":{},"transient":{"cluster":{"routing":{"allocation":{"exclude":{"_name":"10.0.0.99"}}}}}}`,
-// 	}
+// TestSetSetting Func is an integration test for all things that use the SetSetting functionality.
+func TestSetSettings(t *testing.T) {
 
-// 	host, port, ts := setupTestServers(t, []*ServerSetup{getSetup, putSetup})
-// 	defer ts.Close()
+	tt := []struct {
+		Name        string
+		Method      string
+		Body        string
+		GetResponse string
+		PutResponse string
+		Setting     string
+		SetValue    string
+		HTTPStatus  int
+		OldValue    string
+	}{
+		{
+			// Tests for behavior with existing transient setting.
+			Name:        "Existing Transient Setting",
+			GetResponse: `{"persistent":{},"transient":{"cluster":{"routing":{"allocation":{"exclude":{"_name":"10.0.0.2"}}}}}}`,
+			Body:        `{"transient":{"cluster.routing.allocation.exclude._name":"10.0.0.99"}}`,
+			PutResponse: `{"persistent":{},"transient":{"cluster":{"routing":{"allocation":{"exclude":{"_name":"10.0.0.99"}}}}}}`,
+			Setting:     "cluster.routing.allocation.exclude._name",
+			SetValue:    "10.0.0.99",
+			OldValue:    "10.0.0.2",
+		},
 
-// 	oldValue, newValue, err := SetSetting(host, port, "cluster.routing.allocation.exclude._name", "10.0.0.99")
+		{
+			// Tests for behavior with existing persistent settings.
+			Name:        "Existing Persistent Setting",
+			GetResponse: `{"transient":{},"persistent":{"cluster":{"routing":{"allocation":{"exclude":{"_name":"10.0.0.2"}}}}}}`,
+			Body:        `{"transient":{"cluster.routing.allocation.exclude._name":"10.0.0.99"}}`,
+			PutResponse: `{"persistent":{},"transient":{"cluster":{"routing":{"allocation":{"exclude":{"_name":"10.0.0.99"}}}}}}`,
+			Setting:     "cluster.routing.allocation.exclude._name",
+			SetValue:    "10.0.0.99",
+			OldValue:    "10.0.0.2",
+		},
 
-// 	if err != nil {
-// 		t.Fatalf("Expected error to be nil, %s", err)
-// 	}
+		{
+			// Tests for behavior with NO existing persistent settings.
+			Name:        "No existing settings",
+			GetResponse: `{"transient":{},"persistent":{}}`,
+			Body:        `{"transient":{"cluster.routing.allocation.exclude._name":"10.0.0.99"}}`,
+			PutResponse: `{"persistent":{},"transient":{"cluster":{"routing":{"allocation":{"exclude":{"_name":"10.0.0.99"}}}}}}`,
+			Setting:     "cluster.routing.allocation.exclude._name",
+			SetValue:    "10.0.0.99",
+			OldValue:    "",
+		},
+	}
 
-// 	if oldValue != "10.0.0.2" {
-// 		t.Fatalf("Unexpected old value, got %s", oldValue)
-// 	}
+	for _, x := range tt {
+		t.Run(fmt.Sprintf("%s", x.Name), func(st *testing.T) {
 
-// 	if newValue != "10.0.0.99" {
-// 		t.Fatalf("Unexpected new value, got %s", newValue)
-// 	}
-// }
+			getSetup := &ServerSetup{
+				Method:   "GET",
+				Path:     "/_cluster/settings",
+				Response: x.GetResponse,
+			}
+			putSetup := &ServerSetup{
+				Method:   "PUT",
+				Path:     "/_cluster/settings",
+				Body:     x.Body,
+				Response: x.PutResponse,
+			}
 
-// func TestSetSetting_NoExistingSetting(t *testing.T) {
-// 	getSetup := &ServerSetup{
-// 		Method:   "GET",
-// 		Path:     "/_cluster/settings",
-// 		Response: `{"transient":{},"persistent":{}}`,
-// 	}
-// 	putSetup := &ServerSetup{
-// 		Method:   "PUT",
-// 		Path:     "/_cluster/settings",
-// 		Body:     `{"transient":{"cluster.routing.allocation.exclude._name":"10.0.0.99"}}`,
-// 		Response: `{"persistent":{},"transient":{"cluster":{"routing":{"allocation":{"exclude":{"_name":"10.0.0.99"}}}}}}`,
-// 	}
+			host, port, ts := setupTestServers(t, []*ServerSetup{getSetup, putSetup})
+			defer ts.Close()
 
-// 	host, port, ts := setupTestServers(t, []*ServerSetup{getSetup, putSetup})
-// 	defer ts.Close()
+			oldSetting, newSetting, err := SetSetting(host, port, x.Setting, x.SetValue)
 
-// 	oldValue, newValue, err := SetSetting(host, port, "cluster.routing.allocation.exclude._name", "10.0.0.99")
+			if err != nil {
+				st.Errorf("Expected error to be nil, %s", err)
+			}
 
-// 	if err != nil {
-// 		t.Fatalf("Expected error to be nil, %s", err)
-// 	}
+			if oldSetting != x.OldValue {
+				st.Errorf("Unexpected old value, got %s", oldSetting)
+			}
 
-// 	if oldValue != "" {
-// 		t.Fatalf("Unexpected old value, got %s", oldValue)
-// 	}
+			if newSetting != "10.0.0.99" {
+				st.Errorf("Unexpected new value, got %s", newSetting)
+			}
 
-// 	if newValue != "10.0.0.99" {
-// 		t.Fatalf("Unexpected new value, got %s", newValue)
-// 	}
-// }
+		})
+	}
+}
 
+// TestSetSetting Func is an integration test for all things that use the SetAllocation functionality.
+func TestAllocationSettings(t *testing.T) {
+
+	tt := []struct {
+		Name     string
+		Path     string
+		Body     string
+		Response string
+		Setting  string
+		Expected string
+	}{
+		{
+			// Allocation Enable.
+			Name:     "Allocation Enable",
+			Body:     `{"transient":{"cluster.routing.allocation.enable":"all"}}`,
+			Response: `{"persistent":{},"transient":{"cluster":{"routing":{"allocation":{"enable": "all"}}}}}`,
+			Setting:  "enable",
+			Expected: "all",
+		},
+
+		{
+			// Allocation Disable.
+			Name:     "Allocation Disable",
+			Body:     `{"transient":{"cluster.routing.allocation.enable":"none"}}`,
+			Response: `{"persistent":{},"transient":{"cluster":{"routing":{"allocation":{"enable": "none"}}}}}`,
+			Setting:  "disable",
+			Expected: "none",
+		},
+	}
+
+	for _, x := range tt {
+		t.Run(fmt.Sprintf("%s", x.Name), func(st *testing.T) {
+
+			testSetup := &ServerSetup{
+				Method:   "PUT",
+				Path:     "/_cluster/settings",
+				Body:     x.Body,
+				Response: x.Response,
+			}
+
+			host, port, ts := setupTestServers(t, []*ServerSetup{testSetup})
+			defer ts.Close()
+
+			resp := SetAllocation(host, port, x.Setting)
+
+			if resp != x.Expected {
+				st.Errorf("Unexpected response, got %s", resp)
+			}
+
+		})
+	}
+}
 func TestSetSetting_BadRequest(t *testing.T) {
 	getSetup := &ServerSetup{
 		Method:   "GET",
@@ -399,42 +449,6 @@ func TestSetSetting_BadRequest(t *testing.T) {
 
 	if err.Error() != fmt.Sprintf("Bad HTTP Status of 400 from Elasticsearch: %s", putSetup.Response) {
 		t.Fatalf("Unexpected error message, %s", err)
-	}
-}
-
-func TestSetAllocation_Enable(t *testing.T) {
-	testSetup := &ServerSetup{
-		Method:   "PUT",
-		Path:     "/_cluster/settings",
-		Body:     `{"transient":{"cluster.routing.allocation.enable":"all"}}`,
-		Response: `{"persistent":{},"transient":{"cluster":{"routing":{"allocation":{"enable": "all"}}}}}`,
-	}
-
-	host, port, ts := setupTestServers(t, []*ServerSetup{testSetup})
-	defer ts.Close()
-
-	resp := SetAllocation(host, port, "enable")
-
-	if resp != "all" {
-		t.Fatalf("Unexpected response, got %s", resp)
-	}
-}
-
-func TestSetAllocation_Disable(t *testing.T) {
-	testSetup := &ServerSetup{
-		Method:   "PUT",
-		Path:     "/_cluster/settings",
-		Body:     `{"transient":{"cluster.routing.allocation.enable":"none"}}`,
-		Response: `{"persistent":{},"transient":{"cluster":{"routing":{"allocation":{"enable": "none"}}}}}`,
-	}
-
-	host, port, ts := setupTestServers(t, []*ServerSetup{testSetup})
-	defer ts.Close()
-
-	resp := SetAllocation(host, port, "disable")
-
-	if resp != "none" {
-		t.Fatalf("Unexpected response, got %s", resp)
 	}
 }
 
