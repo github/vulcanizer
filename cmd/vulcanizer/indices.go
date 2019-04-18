413098632929fc5989d344392e0a314466d17f77
Ruiwen Chua <ruiwen@thoughtmonkeys.com>
2019-04-18 16:32:09 +0800

    Add subcommands 'open' and 'close' to 'indices'/'index' to support open and closing indices

--- cmd/vulcanizer/indices.go
+++ cmd/vulcanizer/indices.go
@@ -11,6 +11,8 @@ import (
 
 func init() {
 	rootCmd.AddCommand(cmdIndices)
+	cmdIndices.AddCommand(cmdOpen)
+	cmdIndices.AddCommand(cmdClose)
 }
 
 var cmdIndices = &cobra.Command{
@@ -56,5 +58,41 @@ var cmdIndices = &cobra.Command{
 
 		table := renderTable(rows, header)
 		fmt.Println(table)
+	},
+}
+
+var cmdOpen = &cobra.Command{
+	Use:   "open",
+	Short: "Open the given index/indices",
+	Long:  `Given a name or pattern, opens the index/indices`,
+	Args:  cobra.ExactArgs(1),
+	Run: func(cmd *cobra.Command, args []string) {
+		host, port, auth := getConfiguration()
+		v := vulcanizer.NewClient(host, port)
+		v.Auth = auth
+
+		err := v.OpenIndex(args[0])
+		if err != nil {
+			fmt.Printf("Error opening index/indices: %s - %s\n", args[0], err)
+			os.Exit(1)
+		}
+	},
+}
+
+var cmdClose = &cobra.Command{
+	Use:   "close",
+	Short: "Close the given index/indices",
+	Long:  `Given a name or pattern, closes the index/indices`,
+	Args:  cobra.ExactArgs(1),
+	Run: func(cmd *cobra.Command, args []string) {
+		host, port, auth := getConfiguration()
+		v := vulcanizer.NewClient(host, port)
+		v.Auth = auth
+
+		err := v.CloseIndex(args[0])
+		if err != nil {
+			fmt.Printf("Error opening index/indices: %s - %s\n", args[0], err)
+			os.Exit(1)
+		}
 	},
 }
