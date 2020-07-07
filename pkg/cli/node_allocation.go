340afa2a594c653816c372d634535a6c0f986230
Leo Palmer Sunmo <leo.sunmo@gmail.com>
2020-07-07 13:20:22 +1200

    Adds a short flag for nodeallocations
    Makes the information fit a smaller screen and reorders it a bit for clarity.

--- pkg/cli/node_allocation.go
+++ pkg/cli/node_allocation.go
@@ -8,7 +8,10 @@ import (
 	"github.com/spf13/cobra"
 )
 
+var shortTableOutput bool
+
 func init() {
+	cmdNodeAllocations.Flags().BoolVarP(&shortTableOutput, "short", "s", false, "Shorter, more compact table output")
 	rootCmd.AddCommand(cmdNodeAllocations)
 }
 
@@ -27,29 +30,54 @@ var cmdNodeAllocations = &cobra.Command{
 			os.Exit(1)
 		}
 
-		header := []string{"Master", "Role", "Name", "Disk Avail", "Disk Indices", "Disk Percent", "Disk Total", "Disk Used", "Shards", "Ip", "Id", "JDK", "Version"}
-		rows := [][]string{}
-		sort.Slice(nodes, func(i, j int) bool {
-			return nodes[i].Name < nodes[j].Name
-		})
-		for _, node := range nodes {
-			row := []string{
-				node.Master,
-				node.Role,
-				node.Name,
-				node.DiskAvail,
-				node.DiskIndices,
-				node.DiskPercent,
-				node.DiskTotal,
-				node.DiskUsed,
-				node.Shards,
-				node.Ip,
-				node.Id,
-				node.Jdk,
-				node.Version,
-			}
+		var header []string
+		var rows [][]string
+		if shortTableOutput {
+			header = []string{"Role", "Name", "Avail", "Used", "Total", "%", "Indices", "Shards", "Ip"}
+			rows = [][]string{}
+			sort.Slice(nodes, func(i, j int) bool {
+				return nodes[i].Name < nodes[j].Name
+			})
+			for _, node := range nodes {
+				row := []string{
+					fmt.Sprintf("%s%s", node.Master, node.Role),
+					node.Name,
+					node.DiskAvail,
+					node.DiskUsed,
+					node.DiskTotal,
+					node.DiskPercent,
+					node.DiskIndices,
+					node.Shards,
+					node.Ip,
+				}
 
-			rows = append(rows, row)
+				rows = append(rows, row)
+			}
+		} else {
+			header = []string{"Master", "Role", "Name", "Disk Avail", "Disk Indices", "Disk Percent", "Disk Total", "Disk Used", "Shards", "Ip", "Id", "JDK", "Version"}
+			rows = [][]string{}
+			sort.Slice(nodes, func(i, j int) bool {
+				return nodes[i].Name < nodes[j].Name
+			})
+			for _, node := range nodes {
+				row := []string{
+					node.Master,
+					node.Role,
+					node.Name,
+					node.DiskAvail,
+					node.DiskIndices,
+					node.DiskPercent,
+					node.DiskTotal,
+					node.DiskUsed,
+					node.Shards,
+					node.Ip,
+					node.Id,
+					node.Jdk,
+					node.Version,
+				}
+
+				rows = append(rows, row)
+			}
 		}
 
 		table := renderTable(rows, header)
